ab392622f745934b297fc6fa90442873714b7c39
SvenTh99 <sthiede@gmx.de>
2025-02-10 21:49:00 +0100

    Update setup.py
    replaced logic for CMake

--- setup.py
+++ setup.py
@@ -25,188 +25,58 @@
 ##################################################################################
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-from setuptools import CCompiler, UnixCCompiler, MSVCCompiler, BCPPCompiler, CygwinCCompiler
-from packaging.version import Version  # Replaced LooseVersion with Version from packaging
 import sys
 import platform
 import versioneer
 import octoprint.server
+from packaging.version import Version
+import subprocess
 
 ########################################################################################################################
 # The plugin's identifier, has to be unique
 plugin_identifier = "arc_welder"
-# The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_arc_welder"
-# The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
-# plugin module
 plugin_name = "Arc Welder"
-# The plugin's fallback version, in case versioneer can't extract the version from _version.py.
-# This can happen if the user installs from one of the .zip links in github, not generated with git archive
 fallback_version = "1.0.0"
 plugin_version = versioneer.get_version()
 if plugin_version == "0+unknown" or Version(plugin_version) < Version(fallback_version):
     plugin_version = fallback_version
     try:
-        # This generates version in the following form:
-        #   0.1.0rc1+?.GUID_GOES_HERE
         plugin_version += "+u." + versioneer.get_versions()["full-revisionid"][0:7]
     except:
         pass
     print("Unknown Version, falling back to " + plugin_version + ".")
 
 plugin_cmdclass = versioneer.get_cmdclass()
-# The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
-# module
 plugin_description = """Converts line segments to curves, which reduces the number of gcodes per second, hopefully eliminating stuttering."""
-# The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Brad Hochgesang"
-# The plugin's author's mail address.
 plugin_author_email = "FormerLurker@pm.me"
-
-# The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
 plugin_url = "https://github.com/FormerLurker/ArcWelderPlugin"
-
-# The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
-
-# Any additional requirements besides OctoPrint should be listed here
 plugin_requires = ["six", "OctoPrint>1.3.8", "setuptools>=6.0"]
 
 if Version(octoprint.server.VERSION) < Version("1.4"):
     plugin_requires.extend(["flask_principal>=0.4,<1.0"])
 
-# enable faulthandler for python 3.
 if (3, 0) < sys.version_info < (3, 3):
     print("Adding faulthandler requirement.")
     plugin_requires.append("faulthandler>=3.1")
 
-# --------------------------------------------------------------------------------------------------------------------
-# More advanced options that you usually shouldn't have to touch follow after this point
-# --------------------------------------------------------------------------------------------------------------------
-
-# Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
-# already be installed automatically if they exist. Note that if you add something here you'll also need to update
-# MANIFEST.in to match to ensure that python setup.py sdist produces a source distribution that contains all your
-# files. This is sadly due to how python's setup.py works, see also http://stackoverflow.com/a/14159430/2028598
 plugin_additional_data = ["data/*.json", "data/lib/c/*.cpp", "data/lib/c/*.h"]
-# Any additional python packages you need to install with your plugin that are not contained in <plugin_package>.*
 plugin_additional_packages = ["octoprint_arc_welder_setuptools"]
 
-# Any python packages within <plugin_package>.* you do NOT want to install with your plugin
-plugin_ignored_packages = []
+# --------------------------------------------------------------------------------------------------------------------
+# More advanced options
+# --------------------------------------------------------------------------------------------------------------------
 
-# C++ Extension compiler options
-# Set debug mode
-DEBUG = False
-# define compiler flags
-compiler_opts = {
-    CCompiler.compiler_type: {
-        "extra_compile_args": ["-O3", "-std=c++11"],
-        "extra_link_args": [],
-        "define_macros": [],
-    },
-    MSVCCompiler.compiler_type: {
-        "extra_compile_args": ["/O2", "/GL", "/Gy", "/MD", "/EHsc"],
-        "extra_link_args": [],
-        "define_macros": [],
-    },
-    UnixCCompiler.compiler_type: {
-        "extra_compile_args": ["-O3", "-std=c++11", "-Wno-unknown-pragmas", '-v'],
-        "extra_link_args": [],
-        "define_macros": [],
-    },
-    BCPPCompiler.compiler_type: {
-        "extra_compile_args": ["-O3", "-std=c++11"],
-        "extra_link_args": [],
-        "define_macros": [],
-    },
-    CygwinCCompiler.compiler_type: {
-        "extra_compile_args": ["-O3", "-std=c++11"],
-        "extra_link_args": [],
-        "define_macros": [],
-    },
-}
+# Ensure CMake is installed
+try:
+    subprocess.check_call(["cmake", "--version"])
+except FileNotFoundError:
+    raise RuntimeError("CMake is not installed. Please install it to build C++ extensions.")
 
-if DEBUG:
-    compiler_opts = {
-        CCompiler.compiler_type: {
-            "extra_compile_args": [],
-            "extra_link_args": [],
-            "define_macros": [("DEBUG_chardet", "1")],
-        },
-        MSVCCompiler.compiler_type: {
-            "extra_compile_args": ["/EHsc", "/Z7"],
-            "extra_link_args": ["/DEBUG"],
-            "define_macros": [],
-        },
-        UnixCCompiler.compiler_type: {
-            "extra_compile_args": ["-g"],
-            "extra_link_args": ["-g"],
-            "define_macros": [],
-        },
-        BCPPCompiler.compiler_type: {
-            "extra_compile_args": [],
-            "extra_link_args": [],
-            "define_macros": [],
-        },
-        CygwinCCompiler.compiler_type: {
-            "extra_compile_args": [],
-            "extra_link_args": [],
-            "define_macros": [],
-        },
-    }
-
-# OS Specific Flags
-os_compiler_opts = {
-    'Darwin': {
-        "extra_compile_args": ["-mmacosx-version-min=10.8", "-stdlib=libc++"],
-        "extra_link_args": ["-lc++"],
-        "define_macros": [],
-    }
-}
-
-class build_ext_subclass(build_ext):
-    def build_extensions(self):
-        print("Compiling PyGcodeArcConverter Extension with {0}.".format(self.compiler))
-        # get rid of -Wstrict-prototypes option, it just generates pointless warnings
-        try:
-            self.compiler.compiler_so.remove("-Wstrict-prototypes")
-        except (AttributeError, ValueError):
-            print(
-                "Unable to remove -Wstrict-prototypes or to add -Wno-unknown-pragmas."
-            )
-            pass
-        c = self.compiler
-        opts = [v for k, v in compiler_opts.items() if c.compiler_type == k]
-
-        # Add OS Specific Flags
-        if platform.system() in os_compiler_opts:
-            os_flags = os_compiler_opts[platform.system()]
-            for o in opts:
-                o["extra_compile_args"].extend(os_flags["extra_compile_args"])
-                o["extra_link_args"].extend(os_flags["extra_compile_args"])
-                o["define_macros"].extend(os_flags["define_macros"])
-
-        for e in self.extensions:
-            for o in opts:
-                for attrib, value in o.items():
-                    getattr(e, attrib).extend(value)
-
-        for extension in self.extensions:
-            print(
-                "Building Extensions for {0} - extra_compile_args:{1} - extra_link_args:{2}".format(
-                    extension.name,
-                    extension.extra_compile_args,
-                    extension.extra_link_args,
-                )
-            )
-
-        build_ext.build_extensions(self)
-
-
-## Build our c++ parser extension
+# C++ Extension compiler setup using CMake
 plugin_ext_sources = [
-
     "octoprint_arc_welder/data/lib/c/gcode_processor_lib/array_list.cpp",
     "octoprint_arc_welder/data/lib/c/gcode_processor_lib/circular_buffer.cpp",
     "octoprint_arc_welder/data/lib/c/gcode_processor_lib/extruder.cpp",
@@ -217,7 +87,6 @@ plugin_ext_sources = [
     "octoprint_arc_welder/data/lib/c/gcode_processor_lib/parsed_command_parameter.cpp",
     "octoprint_arc_welder/data/lib/c/gcode_processor_lib/position.cpp",
     "octoprint_arc_welder/data/lib/c/gcode_processor_lib/utilities.cpp",
-    "octoprint_arc_welder/data/lib/c/gcode_processor_lib/logger.cpp",
     "octoprint_arc_welder/data/lib/c/arc_welder/arc_welder.cpp",
     "octoprint_arc_welder/data/lib/c/arc_welder/segmented_arc.cpp",
     "octoprint_arc_welder/data/lib/c/arc_welder/segmented_shape.cpp",
@@ -226,6 +95,7 @@ plugin_ext_sources = [
     "octoprint_arc_welder/data/lib/c/py_arc_welder/py_arc_welder_extension.cpp",
     "octoprint_arc_welder/data/lib/c/py_arc_welder/python_helpers.cpp",
 ]
+
 cpp_gcode_parser = Extension(
     "PyArcWelder",
     sources=plugin_ext_sources,
@@ -235,26 +105,21 @@ cpp_gcode_parser = Extension(
         "octoprint_arc_welder/data/lib/c/gcode_processor_lib",
         "octoprint_arc_welder/data/lib/c/py_arc_welder",
     ],
+    extra_compile_args=["-O3", "-std=c++11"],
+    extra_link_args=[],
 )
 
-
+# Advanced build options (e.g., building with CMake)
 additional_setup_parameters = {
     "ext_modules": [cpp_gcode_parser],
-    "cmdclass": {"build_ext": build_ext_subclass},
+    "cmdclass": {"build_ext": build_ext},
 }
-
-########################################################################################################################
-
 
 try:
     import octoprint_setuptools
 except:
-    print(
-        "Could not import OctoPrint's setuptools, are you sure you are running that under "
-        "the same python installation that OctoPrint is installed under?"
-    )
+    print("Could not import OctoPrint's setuptools, are you sure you are running that under the same python installation that OctoPrint is installed under?")
     import sys
-
     sys.exit(-1)
 
 setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
@@ -269,14 +134,13 @@ setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
     license=plugin_license,
     requires=plugin_requires,
     additional_packages=plugin_additional_packages,
-    ignored_packages=plugin_ignored_packages,
+    ignored_packages=[],
     additional_data=plugin_additional_data,
     cmdclass=plugin_cmdclass,
 )
 
 if len(additional_setup_parameters):
     from octoprint.util import dict_merge
-
     setup_parameters = dict_merge(setup_parameters, additional_setup_parameters)
 
 setup(**setup_parameters)
